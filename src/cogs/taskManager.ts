33c2a17b3678347a46c1368c7880e73024b078a8
Tenzyu Masuda <tenzyu.on@gmail.com>
2021-09-18 23:55:50 +0900

    implement task manager

---
+++ src/cogs/taskManager.ts
@@ -0,0 +1,65 @@
+import { RecurrenceRule, scheduleJob } from 'node-schedule';
+import { client } from '../index';
+import * as constant from '../constant';
+import { getTextChannelById } from '../utils';
+import { Message } from 'discord.js';
+
+const EMOJI_SET_TASK = '\uD83D\uDCCC'; // Pushpin
+const EMOJI_UNSET_TASK = '\uD83D\uDC4D'; // Thumbs Up
+
+const setTask = async (message: Message) => {
+  await message.pin();
+  await message.reply(
+    'タスクを登録したよ！\nタスクが完了したら :thumbsup: を付けよう！'
+  );
+};
+
+const unsetTask = async (message: Message) => {
+  await message.unpin();
+  await message.reply(':tada: タスク完了おめでとう！ :tada:');
+  await message.reactions.removeAll();
+};
+
+client.on('messageReactionAdd', async (reaction, user) => {
+  const fixedReaction = reaction.partial ? await reaction.fetch() : reaction;
+  const fixedUser = user.partial ? await user.fetch() : user;
+  const emoji = fixedReaction.emoji.toString();
+
+  if (
+    fixedReaction.message.channel.id !== constant.channelTaskManager ||
+    fixedReaction.message.author?.id !== fixedUser.id ||
+    ![EMOJI_SET_TASK, EMOJI_UNSET_TASK].includes(emoji)
+  ) {
+    return;
+  }
+
+  const fixedMessage = fixedReaction.message.partial
+    ? await fixedReaction.message.fetch()
+    : fixedReaction.message;
+
+  if (emoji === EMOJI_SET_TASK) {
+    await setTask(fixedMessage);
+  } else if (emoji === EMOJI_UNSET_TASK) {
+    await unsetTask(fixedMessage);
+  }
+});
+
+const schedule = new RecurrenceRule();
+schedule.hour = 9; // it means 18:00 in JST
+
+scheduleJob(schedule, async () => {
+  const channel = getTextChannelById(constant.channelTaskManager);
+  const tasks = await channel?.messages.fetchPinned();
+  if (!tasks || tasks.size === 0) return;
+
+  const taskAuthorMentions = Array.from(
+    new Set(tasks.map((task) => task.author.toString()))
+  );
+
+  const taskReminderMessage =
+    taskAuthorMentions.join(' ') +
+    '\n未完了のタスクがあります！' +
+    '\n未完了のタスクはピン留めされています！';
+
+  await channel?.send(taskReminderMessage);
+});
