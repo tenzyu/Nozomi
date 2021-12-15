55e76e4e47ffa0e82e8d35f3646a4e77cd6bcfaf
Tenzyu Masuda <tenzyu.on@gmail.com>
2021-12-16 00:53:27 +0900

    tmp

--- src/cogs/forOwner.ts
+++ src/cogs/forOwner.ts
@@ -2,19 +2,42 @@ import { RecurrenceRule, scheduleJob } from 'node-schedule';
 import { client } from '../index';
 import { getTextChannelById } from '../lib/discordBotUtils';
 import * as constant from '../constant';
+import outdent from 'outdent';
+import { TextChannel } from 'discord.js';
 
-const schedule = new RecurrenceRule();
-schedule.hour = 13; // it means 22:00 PM (JST)
-schedule.minute = 0;
-
-scheduleJob(schedule, async () => {
-  const channel = getTextChannelById('920072102537732136');
-  const reminderMessage = '<@608242236546613259> メラトニンを飲め';
-
-  await channel?.send(reminderMessage);
-});
-
+let channelReminder: TextChannel | undefined;
 client.on('ready', () => {
+  // init
+  channelReminder = getTextChannelById('920072102537732136');
+
   const channel = getTextChannelById(constant.channelLogBotActivity);
   channel?.send('Logged in');
 });
+
+const melatonin = new RecurrenceRule();
+// it means 00:00 PM (JST)
+melatonin.hour = 15;
+melatonin.minute = 0;
+
+scheduleJob(melatonin, async () => {
+  const reminderMessage = outdent`
+  <@608242236546613259>
+  まだ寝ていないならメラトニンを飲め
+  `;
+  await channelReminder?.send(reminderMessage);
+});
+
+const reviewNotes = new RecurrenceRule();
+// it means 15:00, 00:00 PM (JST)
+reviewNotes.hour = [6, 15];
+reviewNotes.minute = 0;
+
+scheduleJob(reviewNotes, async () => {
+  const reminderMessage = outdent`
+  <@608242236546613259>
+  ノートを見ろ
+  `;
+  await channelReminder?.send(reminderMessage);
+});
+
+// 不完全な実装
