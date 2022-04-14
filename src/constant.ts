de8ab21f508637fad5a5a9f2a11ca2044d4e3a0c
Tenzyu Masuda <tenzyu.on@gmail.com>
2022-04-15 02:32:43 +0900

    add some constant

--- src/constant.ts
+++ src/constant.ts
@@ -6,12 +6,8 @@ config({ path: join(__dirname, '../.env') })
 // config
 export const discordBotToken = process.env.DISCORD_BOT_TOKEN
 export const mainGuildId = '888738059036798986'
-export const discordBotToken = process.env.DISCORD_BOT_TOKEN;
-export const mainGuildId = '888738059036798986';
-export const channelTaskManager = '888745950649155604';
-export const channelAssignList = '889097552912334909';
-export const channelLogBotActivity = '920073333117186068';
-export const roleEveryone2Id = '927501472046940190';
+export const categoryActive = '921809230783209542'
+export const categoryNonActive = '964209729842847869'
 export const channelTaskManager = '888745950649155604'
 export const channelAssignList = '889097552912334909'
 export const channelLogBotActivity = '920073333117186068'
