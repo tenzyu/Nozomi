f0efe52693a0555c7b9b81bbb33e9bce11787700
Tenzyu Masuda <tenzyu.on@gmail.com>
2022-04-15 04:39:15 +0900

    add automation for channel management

---
+++ src/cogs/textChannelManager.ts
@@ -0,0 +1,42 @@
+import * as constant from '../constant'
+import { Message } from 'discord.js'
+import { client } from '..'
+import { RecurrenceRule, scheduleJob } from 'node-schedule'
+import {
+  getCategoryChannel,
+  getLastMessageOrNull,
+  isCategoryChannel,
+  isTextChannel
+} from '../lib/discordBotUtils'
+
+client.on('messageCreate', async (message: Message) => {
+  if (message.author.bot) return
+  if (!isTextChannel(message.channel)) return
+  if (!isCategoryChannel(message.channel.parent)) return
+  if (message.channel.parent.id !== constant.categoryInactive) return
+
+  message.channel.edit({ parent: constant.categoryActive })
+})
+
+// every minute
+const schedule = new RecurrenceRule()
+schedule.second = 0
+
+scheduleJob(schedule, async () => {
+  const active = await getCategoryChannel(constant.categoryActive)
+  const inactive = await getCategoryChannel(constant.categoryInactive)
+  const channels = active.children.filter(isTextChannel).toJSON()
+
+  for (const channel of channels) {
+    const lastMessage = await getLastMessageOrNull(channel)
+    if (lastMessage === null) continue
+
+    const now = new Date()
+    const last = lastMessage.editedAt || lastMessage.createdAt
+    const diff = Math.abs(now.getTime() - last.getTime())
+    const hour = 1000 * 60 * 60
+    const diffHour = Math.floor(diff / hour)
+
+    if (diffHour > 6) channel.edit({ parent: inactive })
+  }
+})
--- src/lib/discordBotUtils.ts
+++ src/lib/discordBotUtils.ts
@@ -1,13 +1,52 @@
-import { TextChannel } from 'discord.js'
+import { AnyChannel, CategoryChannel, TextChannel } from 'discord.js'
 import { client } from '../index'
 
-export const isTextChannel = (channel: unknown): channel is TextChannel => {
+export const isTextChannel = (
+  channel: AnyChannel | null
+): channel is TextChannel => {
   return channel instanceof TextChannel
 }
 
+// TODO:
 export const getTextChannelById = (id: string) => {
   const channel = client.channels.resolve(id)
   if (isTextChannel(channel)) {
     return channel
   }
+}
+
+export const getTextChannel = async (id: string) => {
+  const channel = await client.channels.fetch(id)
+
+  if (isTextChannel(channel)) {
+    return channel
+  } else {
+    throw new Error('TextChannel does not exist')
+  }
+}
+
+export const isCategoryChannel = (
+  channel: AnyChannel | null
+): channel is CategoryChannel => {
+  return channel instanceof CategoryChannel
+}
+
+export const getCategoryChannel = async (id: string) => {
+  const channel = await client.channels.fetch(id)
+
+  if (isCategoryChannel(channel)) {
+    return channel
+  } else {
+    throw new Error('CategoryChannel does not exist')
+  }
+}
+
+export const getLastMessageOrNull = async (channel: TextChannel) => {
+  const messages = await channel.messages.fetch({ limit: 1 }).catch(() => null)
+  if (messages === null) return null
+
+  const lastMessage = messages.first()
+  if (lastMessage === undefined) return null
+
+  return lastMessage
 }
