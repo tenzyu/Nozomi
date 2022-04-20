7453fdf7d225526d63d71a128a7e5e0a795a27dd
tenzyu <tenzyu.on@gmail.com>
2022-04-20 22:37:19 +0900

    chore: make better configuration)

--- src/constant.ts
+++ src/constant.ts
@@ -5,10 +5,15 @@ config({ path: join(__dirname, '../.env') })
 
 // config
 export const discordBotToken = process.env.DISCORD_BOT_TOKEN
-export const mainGuildId = '888738059036798986'
-export const categoryActive = '921809230783209542'
-export const categoryInactive = '964203079287337050'
-export const channelTaskManager = '888745950649155604'
-export const channelAssignList = '889097552912334909'
-export const channelLogBotActivity = '920073333117186068'
-export const roleEveryone2Id = '927501472046940190'
+export const mainGuildId = process.env.mainGuildId ?? '888738059036798986'
+export const categoryActive = process.env.categoryActive ?? '921809230783209542'
+export const categoryInactive =
+  process.env.categoryInactive ?? '964203079287337050'
+export const channelTaskManager =
+  process.env.channelTaskManager ?? '888745950649155604'
+export const channelAssignList =
+  process.env.channelAssignList ?? '889097552912334909'
+export const channelLogBotActivity =
+  process.env.channelLogBotActivity ?? '920073333117186068'
+export const roleEveryone2Id =
+  process.env.roleEveryone2Id ?? '927501472046940190'
